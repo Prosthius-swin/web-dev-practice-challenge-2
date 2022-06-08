69b1bdb958071043489c9da3df7ca862e0e5522d
Callum Hopkins <103919468@student.swin.edu.au>
2022-06-08 14:13:35 +1000

    Sounds play on btn press

---
+++ script.js
@@ -0,0 +1,54 @@
+const boom = new Audio('./Drumkit_Sounds-master/boom.wav');
+const clap = new Audio('./Drumkit_Sounds-master/clap.wav');
+const hihat = new Audio('./Drumkit_Sounds-master/hihat.wav');
+const kick = new Audio('./Drumkit_Sounds-master/kick.wav');
+const openhat = new Audio('./Drumkit_Sounds-master/openhat.wav');
+const ride = new Audio('./Drumkit_Sounds-master/ride.wav');
+const snare = new Audio('./Drumkit_Sounds-master/snare.wav');
+const tink = new Audio('./Drumkit_Sounds-master/tink.wav');
+const tom = new Audio('./Drumkit_Sounds-master/tom.wav');
+
+function playBoom()
+{
+    boom.play();
+}
+
+function playClap()
+{
+    clap.play();
+}
+
+function playHihat()
+{
+    hihat.play();
+}
+
+function playKick()
+{
+    kick.play();
+}
+
+function playOpenhat()
+{
+    openhat.play();
+}
+
+function playRide()
+{
+    ride.play();
+}
+
+function playSnare()
+{
+    snare.play();
+}
+
+function playTink()
+{
+    tink.play();
+}
+
+function playTom()
+{
+    tom.play();
+}
